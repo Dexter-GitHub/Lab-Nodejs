318cfd3d7fbfe002fb4aa1a386eaef31cf3b8682
Dexter <mystyle1057@gmail.com>
2023-07-14 09:39:47 +0900

    m file

--- syntax/function.js
+++ syntax/function.js
@@ -1,7 +1,8 @@
 console.log(Math.round(1.6));
 console.log(Math.round(1.4));
-sum(2, 4);
 
 function sum(first, second) {
-    console.log(first+second);
+    return first+second;
 }
+
+console.log(sum(2, 4));
